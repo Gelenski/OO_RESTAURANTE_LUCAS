830fa819366c22c339d9e9943c9e7a0891060f67
Lucas Gelenski <gelenskilucas@gmail.com>
2024-06-05 22:06:46 -0300

    Criando código inicial do restaurante com POO

--- modelos/restaurante2.py
+++ modelos/restaurante2.py
@@ -1,18 +1,21 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+
+    def __str__(self):
+        return f"{self.nome} | {self.categoria} | {self.ativo}"
 
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = "Praça"
-restaurante_praca.categoria = "Gourmet"
+restaurante_praca = Restaurante("Praça", "Gourmet")
 
-restaurante_pizza = Restaurante()
+# restaurante_praca = Restaurante()
+# restaurante_praca.nome = "Praça"
+# restaurante_praca.categoria = "Gourmet"
 
-restaurantes = [restaurante_praca, restaurante_pizza]
 # print(restaurantes)
 # print(restaurante_praca.ativo)
-print(dir(restaurante_praca))
-print()
+# print(dir(restaurante_praca))
+# print()
 print(vars(restaurante_praca))
