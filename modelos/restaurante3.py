739c328d7160aab8dd5b6f831439958ffb74ec4e
Lucas Gelenski <gelenskilucas@gmail.com>
2024-06-07 12:14:02 -0300

    Decorando os prints

--- modelos/restaurante3.py
+++ modelos/restaurante3.py
@@ -1,21 +1,28 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f"{self.nome} | {self.categoria} | {self.ativo}"
 
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f"Nome do restaurante | Categoria | Ativo")
+        for restaurante in cls.restaurantes:
+            print(f"{restaurante.nome} | {
+                  restaurante.categoria} | {restaurante._ativo}")
+
+    @property
+    def ativo(self):
+        return "☒" if self._ativo else "☐"
+
 
 restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("pizza express", "Italiana")
 
-# restaurante_praca = Restaurante()
-# restaurante_praca.nome = "Praça"
-# restaurante_praca.categoria = "Gourmet"
-
-# print(restaurantes)
-# print(restaurante_praca.ativo)
-# print(dir(restaurante_praca))
-# print()
-print(vars(restaurante_praca))
+Restaurante.listar_restaurantes()
