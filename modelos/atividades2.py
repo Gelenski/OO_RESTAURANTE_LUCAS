445e81f3e8ed115beb84c132ec18e061f91414b3
Lucas Gelenski <gelenskilucas@gmail.com>
2024-06-07 08:10:30 -0300

    Formatando conforme a atividade

--- modelos/atividades2.py
+++ modelos/atividades2.py
@@ -1,27 +1,47 @@
-class Carro:
-    def __init__(self, modelo, cor, ano):
-        self.modelo = modelo
-        self.cor = cor
-        self.ano = ano
+# # * Atividade 1
+# class Carro:
+#     def __init__(self, modelo, cor, ano):
+#         self.modelo = modelo
+#         self.cor = cor
+#         self.ano = ano
 
 
-carro = Carro("Fusca", "Azul", 1974)
+# carro = Carro("Fusca", "Azul", 1974)
 # print(vars(carro))
 
 
-class Restaurante:
-    def __init__(self, nome, categoria, preco):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
-        self.preco = preco
-        self.qualidade = "Ótima"
+# # * Atividade 2
+# class Restaurante:
+#     def __init__(self, nome, categoria, preco, qualidade):
+#         self.nome = nome
+#         self.categoria = categoria
+#         self.ativo = False
+#         self.preco = preco
+#         self.qualidade = qualidade
 
-    def __str__(self):
-        return f"O restaurante {self.nome} serve {self.categoria}."
+#     def __str__(self):
+#         return f"{self.nome} | {self.categoria} | {self.ativo} | {self.preco} | {self.qualidade}"
 
 
-parada1 = Restaurante("Parada 1", "Prato Feito / Buffet", "Barato")
+# parada1 = Restaurante("Parada 1", "Prato Feito / Buffet", "Barato", "Boa")
+# print(vars(parada1))
+# print(parada1)
+
+
+# # * Atividade 3
+# class Restaurante:
+#     def __init__(self, nome, categoria, preco):
+#         self.nome = nome
+#         self.categoria = categoria
+#         self.ativo = False
+#         self.preco = preco
+#         self.qualidade = "Depende"
+
+#     def __str__(self):
+#         return f"{self.nome} | {self.categoria} | {self.ativo} | {self.preco} | {self.qualidade}"
+
+
+# parada1 = Restaurante("Parada 1", "Prato Feito / Buffet", "Barato")
 # print(vars(parada1))
 # print(parada1)
 
@@ -40,4 +60,4 @@ class Cliente:
 paulo = Cliente("Paulo", "Gelenski", False, "Ativo")
 joao = Cliente("João", "Batista", True, "Inativo")
 cleverson = Cliente("Cleverson", "Silva", True, "Ativo")
-# print(f"{paulo}\n{joao}\n{cleverson}")
+print(f"{paulo}\n{joao}\n{cleverson}")
